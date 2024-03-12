9faa18006a104bf1139206a6489e5640edd53c0a
Vladimir Belyaev <vogisa@mail.ru>
2024-03-12 16:46:48 +0300

    added prime vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  imports: {
+    autoImport: false,
+  },
+
+  css: ['~/assets/styles/main.css', 'primevue/resources/themes/aura-light-green/theme.css'],
+
   modules: [
     '@nuxtjs/eslint-module',
+    'nuxt-primevue',
   ],
+
+  primevue: {
+    components: {
+      include: ['DataTable', 'Column', 'ColumnGroup', 'Row'],
+    },
+  },
 })
