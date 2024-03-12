1d864f7f687467f85797d203dc9c54c32c62fa43
Vladimir Belyaev <vogisa@mail.ru>
2024-03-12 20:38:42 +0300

    convert set to array

--- utils/getColors.ts
+++ utils/getColors.ts
@@ -1,5 +1,5 @@
 import type { IUser } from '~/typing/IUser';
 
-export default (users: IUser[]): Set<string> => {
-  return new Set(users.map((user: IUser) => user.color));
+export default (users: IUser[]): string[] => {
+  return Array.from(new Set(users.map((user: IUser) => user.color)));
 };
