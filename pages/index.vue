3f0c89bb92b3fe43293f960bbd55e835ca2af516
Vladimir Belyaev <vogisa@mail.ru>
2024-03-12 19:11:16 +0300

    initial table view

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
-import users from '@/data/users.json';
+import ColorView from '~/components/ColorView';
 
-const route = useRoute();
+import users from '~/data/users.json';
 </script>
 
 <template>
-  <div>
-    <p>
-      {{ users }}
-    </p>
-    <hr>
-    <p>
-      {{ route.query }}
-    </p>
+  <div class="page">
+    <DataTable :value="users" size="large" table-style="min-width: 50rem">
+      <Column field="name" header="Name" />
+      <Column field="age" header="Age" />
+      <Column field="role" header="Role" />
+      <Column field="balance" header="Balance" />
+      <Column field="color" header="Color">
+        <template #body="slotProps">
+          <ColorView :color="slotProps.data.color" />
+        </template>
+      </Column>
+      <Column field="blocked" header="Blocked" />
+    </DataTable>
   </div>
 </template>
+
+<style lang="scss">
+.page {
+  width: 50rem;
+  margin: 0 auto;
+}
+</style>
