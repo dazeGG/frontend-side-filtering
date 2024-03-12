e72f8e0b20c8cab263b532c07fd8ab7acf0a3f10
Vladimir Belyaev <vogisa@mail.ru>
2024-03-13 01:54:22 +0300

    init some filters and the logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,69 @@
 <script setup lang="ts">
+import { computed, reactive } from 'vue';
+
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
+import MultiSelect from 'primevue/multiselect';
+import TriStateCheckbox from 'primevue/tristatecheckbox';
+
+import type { IUser } from '~/typing/IUser';
 
 import ColorView from '~/components/ColorView';
 import BlockedView from '~/components/BlockedView';
 
 import users from '~/data/users.json';
+
+import getColors from '~/utils/getColors';
+
+const filters: { blocked: null | boolean; colors: string[] } = reactive({ blocked: null, colors: [] });
+
+const filteredUsers = computed(() => {
+  return users
+    .filter((user: IUser) => !filters.colors.length || filters.colors.includes(user.color))
+    .filter((user: IUser) => filters.blocked === null || user.blocked === filters.blocked);
+});
+
+const colors = computed(() => getColors(users));
 </script>
 
 <template>
   <div class="page">
-    <DataTable :value="users" size="large" table-style="min-width: 50rem">
-      <Column field="name" header="Name" />
-      <Column field="age" header="Age" />
-      <Column field="role" header="Role" />
-      <Column field="color" header="Color">
+    {{ filters }}
+    <DataTable :value="filteredUsers" striped-rows size="large" filter-display="row">
+      <Column field="name" header="Name" style="width: 6rem" />
+      <Column field="age" header="Age" style="width: 4rem" />
+      <Column field="role" header="Role" style="width: 6rem" />
+      <Column
+        field="color"
+        filter-field="color"
+        :show-filter-menu="false"
+        header="Color"
+      >
+        <template #filter>
+          <MultiSelect
+            v-model="filters.colors"
+            :options="colors"
+            placeholder="Any"
+            class="p-column-filter"
+            :max-selected-labels="1"
+            style="width: 8rem"
+          >
+            <template #option="{ option }">
+              <ColorView :color="option" />
+            </template>
+          </MultiSelect>
+        </template>
         <template #body="{ data: { color } }">
           <ColorView :color="color" />
         </template>
       </Column>
       <Column field="balance" header="Balance" />
-      <Column field="blocked" header="Blocked" data-type="boolean" style="min-width: 6rem">
+      <Column field="blocked" header="Blocked" style="min-width: 6rem">
         <template #body="{ data: { blocked } }">
           <BlockedView :blocked="blocked" />
+        </template>
+        <template #filter>
+          <TriStateCheckbox v-model="filters.blocked" />
         </template>
       </Column>
     </DataTable>
