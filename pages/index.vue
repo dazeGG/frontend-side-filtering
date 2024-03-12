a57d13aba7eafde5bdb36687cdfbb734b585660a
Vladimir Belyaev <vogisa@mail.ru>
2024-03-12 17:17:42 +0300

    created index component

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+import users from '@/data/users.json';
+
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ users }}
+    </p>
+    <hr>
+    <p>
+      {{ route.query }}
+    </p>
+  </div>
+</template>
