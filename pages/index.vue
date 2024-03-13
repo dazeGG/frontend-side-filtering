4afb39e4262b0dfb06c2ff56d7bcadbd21f24cf3
Vladimir Belyaev <vogisa@mail.ru>
2024-03-13 23:41:12 +0300

    integrated AgePicker component to table with filtering logic that works with query

--- pages/index.vue
+++ pages/index.vue
@@ -10,20 +10,35 @@ import TriStateCheckbox from 'primevue/tristatecheckbox';
 import type { ComputedRef } from 'vue';
 import type { IUser } from '~/typing/IUser';
 
+import AgePicker from '~/components/AgePicker.vue';
 import ColorView from '~/components/ColorView';
 import BlockedView from '~/components/BlockedView';
 
 import users from '~/data/users.json';
 
-import getColors from '~/utils/getColors';
+import getMinMaxAges from '~/utils/getMinMaxAges';
 import getRoles from '~/utils/getRoles';
+import getColors from '~/utils/getColors';
 
 const route = useRoute();
 const router = useRouter();
 
-const queryFilters: ComputedRef<{ blocked: null | boolean; colors: string[]; roles: string[] }> = computed(() => {
+const ages = computed(() => getMinMaxAges(users));
+const roles = computed(() => getRoles(users));
+const colors = computed(() => getColors(users));
+
+const queryFilters: ComputedRef<{
+  age: [number, number];
+  roles: string[];
+  colors: string[];
+  blocked: null | boolean
+}> = computed(() => {
+  const age: [number, number] = [ages.value.min, ages.value.max];
   const roles: string[] = [];
   const colors: string[] = [];
+
+  if (route.query['age-min']) { age[0] = parseInt(route.query['age-min']?.toString()) }
+  if (route.query['age-max']) { age[1] = parseInt(route.query['age-max']?.toString()) }
 
   if (route.query.roles) {
     if (typeof route.query.roles === 'string') {
@@ -44,24 +59,50 @@ const queryFilters: ComputedRef<{ blocked: null | boolean; colors: string[]; rol
   }
 
   return {
+    age,
     blocked: route.query.blocked ? route.query.blocked === 'true' : null,
     roles,
     colors,
   };
 });
 
-const filters: { blocked: null | boolean; colors: string[]; roles: string[] } = reactive({ ...queryFilters.value });
+const filters: {
+  age: [number, number];
+  roles: string[];
+  colors: string[];
+  blocked: null | boolean
+} = reactive({ ...queryFilters.value });
 
 const filteredUsers = computed(() => {
+  const ageMin = Math.min(...filters.age);
+  const ageMax = Math.max(...filters.age);
+
   return users
+    .filter((user: IUser) => ageMin <= user.age && user.age <= ageMax)
     .filter((user: IUser) => !queryFilters.value.roles.length || queryFilters.value.roles.includes(user.role))
     .filter((user: IUser) => !queryFilters.value.colors.length || queryFilters.value.colors.includes(user.color))
     .filter((user: IUser) => queryFilters.value.blocked === null || user.blocked === queryFilters.value.blocked);
 });
 
-const roles = computed(() => getRoles(users));
-const colors = computed(() => getColors(users));
+const filterAge = (newAges: [number, number]) => {
+  const query = { ...route.query };
+  const min = Math.min(...newAges);
+  const max = Math.max(...newAges);
 
+  if (min !== ages.value.min) {
+    query['age-min'] = min.toString();
+  } else {
+    delete query['age-min'];
+  }
+
+  if (max !== ages.value.max) {
+    query['age-max'] = max.toString();
+  } else {
+    delete query['age-max'];
+  }
+
+  router.push({ path: '', query });
+};
 const filterRoles = () => router.push({ path: '', query: { ...route.query, roles: filters.roles } });
 const filterColors = () => router.push({ path: '', query: { ...route.query, colors: filters.colors } });
 const filterBlocked = () => {
@@ -81,7 +122,11 @@ const filterBlocked = () => {
   <div class="page">
     <DataTable :value="filteredUsers" striped-rows size="large" filter-display="row" style="width: 60rem">
       <Column field="name" header="Name" style="width: 8rem" />
-      <Column field="age" header="Age" style="width: 12rem" />
+      <Column field="age" header="Age" style="width: 12rem" :show-filter-menu="false">
+        <template #filter>
+          <AgePicker v-model="filters.age" :min="ages.min" :max="ages.max" @update:age="filterAge" />
+        </template>
+      </Column>
       <Column field="role" header="Role" style="width: 12rem" :show-filter-menu="false">
         <template #filter>
           <MultiSelect
