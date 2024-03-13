be2e06649ab26d8e8f118012c49488ae3e078bc6
Vladimir Belyaev <vogisa@mail.ru>
2024-03-13 14:34:46 +0300

    added filtering logic with query support and fixed view

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { computed, reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import MultiSelect from 'primevue/multiselect';
 import TriStateCheckbox from 'primevue/tristatecheckbox';
 
+import type { ComputedRef } from 'vue';
 import type { IUser } from '~/typing/IUser';
 
 import ColorView from '~/components/ColorView';
@@ -14,39 +16,93 @@ import BlockedView from '~/components/BlockedView';
 import users from '~/data/users.json';
 
 import getColors from '~/utils/getColors';
+import getRoles from '~/utils/getRoles';
 
-const filters: { blocked: null | boolean; colors: string[] } = reactive({ blocked: null, colors: [] });
+const route = useRoute();
+const router = useRouter();
+
+const queryFilters: ComputedRef<{ blocked: null | boolean; colors: string[]; roles: string[] }> = computed(() => {
+  const roles: string[] = [];
+  const colors: string[] = [];
+
+  if (route.query.roles) {
+    if (typeof route.query.roles === 'string') {
+      roles.push(route.query.roles);
+    } else {
+      // @ts-ignore
+      route.query.roles.forEach((role: string) => roles.push(role.toString()));
+    }
+  }
+
+  if (route.query.colors) {
+    if (typeof route.query.colors === 'string') {
+      colors.push(route.query.colors);
+    } else {
+      // @ts-ignore
+      route.query.colors.forEach((color: string) => colors.push(color.toString()));
+    }
+  }
+
+  return {
+    blocked: route.query.blocked ? route.query.blocked === 'true' : null,
+    roles,
+    colors,
+  };
+});
+
+const filters: { blocked: null | boolean; colors: string[]; roles: string[] } = reactive({ ...queryFilters.value });
 
 const filteredUsers = computed(() => {
   return users
-    .filter((user: IUser) => !filters.colors.length || filters.colors.includes(user.color))
-    .filter((user: IUser) => filters.blocked === null || user.blocked === filters.blocked);
+    .filter((user: IUser) => !queryFilters.value.roles.length || queryFilters.value.roles.includes(user.role))
+    .filter((user: IUser) => !queryFilters.value.colors.length || queryFilters.value.colors.includes(user.color))
+    .filter((user: IUser) => queryFilters.value.blocked === null || user.blocked === queryFilters.value.blocked);
 });
 
+const roles = computed(() => getRoles(users));
 const colors = computed(() => getColors(users));
+
+const filterRoles = () => router.push({ path: '', query: { ...route.query, roles: filters.roles } });
+const filterColors = () => router.push({ path: '', query: { ...route.query, colors: filters.colors } });
+const filterBlocked = () => {
+  const query = { ...route.query };
+
+  if (typeof filters.blocked === 'boolean') {
+    query.blocked = filters.blocked ? 'true' : 'false';
+  } else {
+    delete query.blocked;
+  }
+
+  router.push({ path: '', query });
+};
 </script>
 
 <template>
   <div class="page">
-    {{ filters }}
-    <DataTable :value="filteredUsers" striped-rows size="large" filter-display="row" style="width: 50rem">
-      <Column field="name" header="Name" style="width: 6rem" />
-      <Column field="age" header="Age" style="width: 4rem" />
-      <Column field="role" header="Role" style="width: 6rem" />
-      <Column
-        field="color"
-        filter-field="color"
-        :show-filter-menu="false"
-        header="Color"
-      >
+    <DataTable :value="filteredUsers" striped-rows size="large" filter-display="row" style="width: 60rem">
+      <Column field="name" header="Name" style="width: 8rem" />
+      <Column field="age" header="Age" style="width: 12rem" />
+      <Column field="role" header="Role" style="width: 12rem" :show-filter-menu="false">
+        <template #filter>
+          <MultiSelect
+            v-model="filters.roles"
+            :options="roles"
+            placeholder="Choose roles"
+            class="p-column-filter"
+            :max-selected-labels="1"
+            @change="filterRoles"
+          />
+        </template>
+      </Column>
+      <Column field="color" header="Color" style="width: 12rem" :show-filter-menu="false">
         <template #filter>
           <MultiSelect
             v-model="filters.colors"
             :options="colors"
-            placeholder="Any"
+            placeholder="Choose colors"
             class="p-column-filter"
             :max-selected-labels="1"
-            style="width: 8rem"
+            @change="filterColors"
           >
             <template #option="{ option }">
               <ColorView :color="option" />
@@ -57,13 +113,13 @@ const colors = computed(() => getColors(users));
           <ColorView :color="color" />
         </template>
       </Column>
-      <Column field="balance" header="Balance" />
-      <Column field="blocked" header="Blocked" style="min-width: 6rem">
+      <Column field="balance" header="Balance" style="width: 8rem" />
+      <Column field="blocked" header="Blocked" style="width: 8rem" :show-filter-menu="false">
         <template #body="{ data: { blocked } }">
           <BlockedView :blocked="blocked" />
         </template>
         <template #filter>
-          <TriStateCheckbox v-model="filters.blocked" />
+          <TriStateCheckbox v-model="filters.blocked" @change="filterBlocked" />
         </template>
       </Column>
     </DataTable>
@@ -72,7 +128,7 @@ const colors = computed(() => getColors(users));
 
 <style lang="scss">
 .page {
-  max-width: 50rem;
+  max-width: 60rem;
   margin: 0 auto;
 }
 </style>
